022d4d1d43df65a06d42be4674af23fcec5a0f61
unknown <luisfelipe.fsk@gmail.com>
2026-06-23 01:30:12 -0300

    fix(): sin/cos formula

--- trig_gen.py
+++ trig_gen.py
@@ -1,19 +1,19 @@
 import math
 
 OFFSET = 32768
+CORRECTION = 2*OFFSET
 BUFFER = 64
-MASK = 0x8000
 
 print("Started.")
 
 with open("rom_cos.hex", "w") as c, open("rom_sin.hex", "w") as s:
     for i in range(BUFFER):
-        cv = int(math.cos(2*math.pi*i/64)*OFFSET)
-        sv = int(math.sin(2*math.pi*i/64)*OFFSET)
-        if cv == OFFSET: cv = OFFSET-1
-        if sv == OFFSET: sv = OFFSET-1
-        mcv = MASK | -cv if cv < 0 else cv
-        msv = MASK | -sv if sv < 0 else sv
+        cv = math.floor(math.cos(2*math.pi*i/BUFFER)*OFFSET + 0.5)
+        sv = math.floor(math.sin(2*math.pi*i/BUFFER)*OFFSET + 0.5)
+        if (cv == 32768): cv = 32767
+        if (sv == 32768): sv = 32767
+        mcv = cv + CORRECTION if cv < 0 else cv
+        msv = sv + CORRECTION if sv < 0 else sv
         c.write(f"{mcv:04X}{'\n' if i != (BUFFER-1) else ''}")
         s.write(f"{msv:04X}{'\n' if i != (BUFFER-1) else ''}")
 
